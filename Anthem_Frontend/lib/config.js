4458065b6215559048a5e01d469b0882323bb85a
codebyArya-bit <jobsforarya2023@gmail.com>
2026-06-12 16:48:21 +0530

    change the config file to 127.0.0.1:8000

--- Anthem_Frontend/lib/config.js
+++ Anthem_Frontend/lib/config.js
@@ -1,7 +1,7 @@
 // For development - point to local backend
-// export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 
 // For Production - will be https://www.anthemgt.com
 // export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://www.anthemgt.com";
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://103.191.132.173:3007";
+// export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://103.191.132.173:3007";
